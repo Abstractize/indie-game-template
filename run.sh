ff89d2eb82c429b7187955d49cad2fc7980df1d6
Abstractize <gababarca@hotmail.com>
2022-06-09 23:36:29 -0600

    [Add] Config File

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Show env vars
-grep -v '^#' .env
-
-# Export env vars
-export $(grep -v '^#' .env | xargs)
-docker build --build-arg UNITY_EMAIL=$UNITY_EMAIL --build-arg UNITY_PASSWORD=$UNITY_PASSWORD --build-arg UNITY_SERIAL=$UNITY_SERIAL .
