ebef5e100fe3d5832284535e1fa061d1985cb186
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2022-06-12 18:13:33 -0600

    Lobby (#11)
    * [Add] Lobby Rooms
    
    * [Update] Ignore

---
+++ Assets/Lobby/RoomData.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomData : MonoBehaviour
+{
+    public string RoomName { get; set; }
+    [SerializeField] TextMeshProUGUI RoomNameAsset;
+    [SerializeField] TextMeshProUGUI RoomPlayerCount;
+    [SerializeField] TextMeshProUGUI MaxPlayerQuantity;
+    [SerializeField] TextMeshProUGUI RoomOpen;
+
+    public void JoinRoom()
+        => PhotonNetwork.JoinRoom(RoomName);
+
+    public void Initialize(Photon.Realtime.RoomInfo roomInfo)
+    {
+        RoomName = roomInfo.Name;
+        RoomNameAsset.text = "Room: " + roomInfo.Name;
+        RoomPlayerCount.text = "Players: " + roomInfo.PlayerCount.ToString();
+        MaxPlayerQuantity.text = "MaxPlayers: " + (4).ToString();
+        RoomOpen.text = roomInfo.IsOpen ? "Open" : "Closed";
+    }
+}
